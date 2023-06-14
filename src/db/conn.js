5562642de2b37b7d6462aeffcef93fa422c872dc
Shivam <shivam66jnp@gmail.com>
2023-06-14 23:33:51 +0530

    added more file

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,13 @@
-const mongoose =require("mongoose");
-mongoose.connect("mongodb://127.0.0.1/ApnaKart",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    serverSelectionTimeoutMS: 5000
-    // serverSelectionTimeoutMS: 5000 // optional
-}).then(()=>{
-    console.log(`connection successful`);
-}).catch((e)=>{
-    console.log(`no connection ${e}`);
+const mongoose = require("mongoose");
+
+const connectionString = "mongodb+srv://shivam66jnp:XYPPYf4gyJf5El4O@cluster0.ru4bvi9.mongodb.net/ApnaKart?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+//   serverSelectionTimeoutMS: 5000
+}).then(() => {
+  console.log("Connection to MongoDB Atlas successful");
+}).catch((error) => {
+  console.log("Error connecting to MongoDB Atlas:", error);
 });
